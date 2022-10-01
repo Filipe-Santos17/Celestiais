0df4aa2ee63b158635560508d00181ebd1577c1e
Filipe <filipemard@gmail.com>
2022-10-01 11:53:15 -0300

    card incompleto

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,12 +1,24 @@
 import * as React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
+import { ContainerView, TittleContainer, ScroolView } from "./Home.style"
+//https://www.youtube.com/watch?v=1tIBMiuWaOg&ab_channel=UnsureProgrammer
 
 export default function Home() {
   return (
-    <View>
-      <Text>
-        Home
-      </Text>
-    </View>
+    <ContainerView>
+      <TittleContainer>Monera</TittleContainer>
+      <ScroolView>
+        <ScrollView
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+        >
+          <View style={{height: 130, width: 130}} >
+            <View style={{flex: 2}}>
+              <Image source={require('../../assets/tardigrada.png')} style={{flex: 1}}/>
+            </View> 
+          </View>
+        </ScrollView>
+      </ScroolView>
+    </ContainerView>
   )
 }
